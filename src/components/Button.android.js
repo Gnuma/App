f5faaca3b4f522f83b0f5d68e1f8b7a415605430
aIacoella <angeloiacoella3@gmail.com>
2019-02-27 20:56:19 +0100

    laptop commit

---
+++ src/components/Button.android.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { StyleSheet, TouchableOpacity, View, TouchableNativeFeedback } from "react-native";
+
+export class Button extends Component {
+  render() {
+    return (
+      <TouchableNativeFeedback
+        onPress={this.props.onPress}
+        background={TouchableNativeFeedback.SelectableBackground()}
+        useForeground={true}>
+      
+      <View 
+        pointerEvents='box-only'
+        style={this.props.style}>
+        {this.props.children}
+      </View>
+      
+      </TouchableNativeFeedback>
+    );
+  }
+}
+
+Button.defaultProps = { disabled: false };
+
+export default Button;
