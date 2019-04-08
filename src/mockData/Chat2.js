a791176e2f44870e3170f47349c8df77a102747f
aIacoella <angeloiacoella@gmail.com>
2019-04-08 17:36:48 +0200

    chat mock data added

--- src/mockData/Chat2.js
+++ src/mockData/Chat2.js
@@ -53,17 +53,161 @@ export const single = {
     },
     {
       id: uuid.v4(),
-      content: "Messsaggio 6",
+      content: "Messsaggio 7",
       timestamp: new Date(2019, 2, 4, 7, 0),
       author: myID
     },
     {
       id: uuid.v4(),
-      content: "Messsaggio 6",
+      content: "Messsaggio 8",
       timestamp: new Date(2019, 2, 4, 6, 0),
       author: myID
     }
   ]
+};
+
+const chat1ID = uuid.v4();
+const toUser1ID = uuid.v4();
+const chat2ID = uuid.v4();
+const toUser2ID = uuid.v4();
+const chat3ID = uuid.v4();
+const toUser3ID = uuid.v4();
+
+export const buyerChatList = {
+  [chat1ID]: {
+    id: chat1ID,
+    UserTO: {
+      id: toUser1ID,
+      username: "Giancarlo",
+      lastVisit: new Date(2019, 2, 4, 12, 0)
+    },
+    status: null,
+    item: {
+      itemID: uuid.v4(),
+      book: {
+        title: "Matematica Colore",
+        authors: "Massimo Massimi"
+      }
+    },
+    messages: [
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 1",
+        timestamp: new Date(2019, 2, 4, 12, 0),
+        author: toUser1ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 2",
+        timestamp: new Date(2019, 2, 4, 11, 0),
+        author: myID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 3",
+        timestamp: new Date(2019, 2, 4, 10, 0),
+        author: toUser1ID
+      }
+    ]
+  },
+
+  [chat2ID]: {
+    id: chat2ID,
+    UserTO: {
+      id: toUser2ID,
+      username: "Marco",
+      lastVisit: new Date(2019, 2, 4, 12, 0)
+    },
+    status: null,
+    item: {
+      itemID: uuid.v4(),
+      book: {
+        title: "Italiano Carrarar",
+        authors: "Massimo AAAA"
+      }
+    },
+    messages: [
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 1",
+        timestamp: new Date(2019, 2, 4, 11, 30),
+        author: toUser2ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 2",
+        timestamp: new Date(2019, 2, 4, 11, 0),
+        author: myID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 3",
+        timestamp: new Date(2019, 2, 4, 10, 0),
+        author: toUser2ID
+      }
+    ]
+  },
+
+  [chat3ID]: {
+    id: chat3ID,
+    UserTO: {
+      id: toUser3ID,
+      username: "Marco Carco",
+      lastVisit: new Date(2019, 2, 4, 12, 0)
+    },
+    status: null,
+    item: {
+      itemID: uuid.v4(),
+      book: {
+        title: "dfgdfg asdas",
+        authors: "Massimo sdfghdfgdfgdfgdfgfghndgh"
+      }
+    },
+    messages: [
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 1",
+        timestamp: new Date(2019, 2, 4, 12, 0),
+        author: toUser3ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 2",
+        timestamp: new Date(2019, 2, 4, 11, 0),
+        author: myID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 3",
+        timestamp: new Date(2019, 2, 4, 10, 0),
+        author: toUser3ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 4",
+        timestamp: new Date(2019, 2, 4, 9, 0),
+        author: toUser3ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 5",
+        timestamp: new Date(2019, 2, 4, 8, 0),
+        author: toUser3ID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 7",
+        timestamp: new Date(2019, 2, 4, 7, 0),
+        author: myID
+      },
+      {
+        id: uuid.v4(),
+        content: "Messsaggio 8",
+        timestamp: new Date(2019, 2, 4, 6, 0),
+        author: myID
+      }
+    ]
+  }
 };
 
 /*
